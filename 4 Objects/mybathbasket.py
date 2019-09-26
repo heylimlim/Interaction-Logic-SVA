3c106fada69ecda7303a377fca418c6ef597ee77
Hyelim Lim <55026823+heylimlim@users.noreply.github.com>
2019-09-26 11:21:48 -0400

    Delete mybathbasket.py

--- 4 Objects/mybathbasket.py
+++
@@ -1,5 +0,0 @@
-# my bath basket
-
-mybathBasket = {"shampoo", "bodywash", "toothpaste", "conditioner", "showerball", "soap", "toothbrush", "facial cleanser", "deodorant", "razor", "tissue"}
-
-print(mybathBasket)
