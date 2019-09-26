34e4b2bdb192861191b6b5bd0c7e822842089519
Hyelim Lim <55026823+heylimlim@users.noreply.github.com>
2019-09-26 11:22:27 -0400

    Add files via upload

---
+++ 3rd week_objects, logic game/4 Objects/mybathbasket.py
@@ -0,0 +1,5 @@
+# my bath basket
+
+mybathBasket = {"shampoo", "bodywash", "toothpaste", "conditioner", "showerball", "soap", "toothbrush", "facial cleanser", "deodorant", "razor", "tissue"}
+
+print(mybathBasket)
